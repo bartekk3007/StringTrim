4286e387cd3b9d41b85dd122f8f1c7d3d5a38568
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-25 23:41:51 +0200

    Add files via upload

---
+++ StringTrim/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace StringTrim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "  Bartek ";
+            string improvedText = text.Trim(' ');
+            Console.WriteLine(improvedText);
+        }
+    }
+}
